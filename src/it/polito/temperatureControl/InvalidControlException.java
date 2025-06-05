8c433f3c8c4e88115f2fc002ebe5b99dc86c6a34
S346265 <S346265@studenti.poltio.it>
2025-06-05 12:17:19 +0200

    Exception adding

--- src/it/polito/temperatureControl/InvalidControlException.java
+++ src/it/polito/temperatureControl/InvalidControlException.java
@@ -1,5 +1,13 @@
 package it.polito.temperatureControl;
 
-public class InvalidControlException extends Throwable{
+@SuppressWarnings("serial")
+public class InvalidControlException extends Exception {
+
+	public InvalidControlException() {
+		super("Error occured");
+	}
+	public InvalidControlException(String msg) {
+		super(msg);
+	}
 
 }
